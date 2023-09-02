472106482e2741ca918416563274be901b906704
user <user@LAPTOP-9KKL2RG2>
2023-09-02 10:52:51 +0800

    加入專案檔案。

---
+++ ShirleyWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShirleyWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
