a6205b16caab5258425552b78f35d0b21ded3ca8
Huang Xuan Yu <showhk61@gmail.com>
2023-10-07 12:37:16 +0800

    Company CRUD

---
+++ Shirley.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,25 @@
+﻿using ShirleyBook.DataAccess.Data;
+using ShirleyBook.DataAccess.Repository.IRepository;
+using ShirleyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShirleyBook.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private ApplicationDbContext _db;
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Company obj)
+        {
+            _db.Companies.Update(obj);
+        }
+    }
+}
---
+++ Shirley.DataAccess/Repository/IRepository/ICompanyRepository.cs
@@ -0,0 +1,14 @@
+﻿using ShirleyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShirleyBook.DataAccess.Repository.IRepository
+{
+    public interface ICompanyRepository : IRepository<Company>
+    {
+        void Update(Company obj);
+    }
+}
